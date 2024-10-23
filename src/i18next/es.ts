160e14810a0e69da3e2f7ca94110f6f14016fdb1
Roberto Bustos <robertobustos@MacBook-Pro-de-Roberto.local>
2024-10-23 05:19:40 -0600

    fixed spanish translation

--- src/i18next/es.ts
+++ src/i18next/es.ts
@@ -1,23 +1,27 @@
 const es = {
-  buttons: {
+  translation: {
+    buttons: {
+      subject: {
+        editLabel: "Editar",
+        deleteLabel: "Borrar",
+        confirmAddLabel: "Agregar",
+        confirmEditLabel: "Guardar",
+        cancelEditLabel: "Cerrar",
+      },
+    },
     subject: {
-      editLabel: "Editar",
-      deleteLabel: "Borrar",
-      confirmAddLabel: "Agregar",
-    },
-  },
-  subject: {
-    editModal: {
-      title: "Editar materia",
-      subtitle: "Escriba un nuevo nombre para esta materia",
-      description:
-        "Este es un texto larguisimo que pude haber generado en la pagina generadora de textos largos o con algun lorem lipsum pero en vez de eso mejor perdere mi tiempo y escribire manualmente todo",
-    },
-    catalogPage: {
-      title: "Catalogo de Materias",
-    },
-    addSubjectForm: {
-      label: "Nueva materia",
+      editModal: {
+        title: "Editar materia",
+        subtitle: "Escriba un nuevo nombre para esta materia",
+        description:
+          "Este es un texto larguisimo que pude haber generado en la pagina generadora de textos largos o con algun lorem lipsum pero en vez de eso mejor perdere mi tiempo y escribire manualmente todo",
+      },
+      catalogPage: {
+        title: "Catalogo de Materias",
+      },
+      addSubjectForm: {
+        label: "Nueva materia",
+      },
     },
   },
 };
