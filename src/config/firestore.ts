151a3ceb81fc0d8dbb81597437df9e35b291618c
Roberto Bustos <robertobustos@MacBook-Pro-de-Roberto.local>
2024-10-26 09:44:05 -0600

    removed secrets from codebase

--- src/config/firestore.ts
+++ src/config/firestore.ts
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-} from "firebase/firestore";
+import { getFirestore, collection } from "firebase/firestore";
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,13 +8,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBIk7hl58jdzI2inrn3fCXkewsdhoNfgWo",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "my-fake-school.firebaseapp.com",
   projectId: "my-fake-school",
   storageBucket: "my-fake-school.appspot.com",
-  messagingSenderId: "867750236804",
-  appId: "1:867750236804:web:f9408e6c02d2406f68a8d1",
-  measurementId: "G-WZ2BV20SG5",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBSAE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
